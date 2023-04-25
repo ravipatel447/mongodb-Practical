273b2f5da93351803374d4ec36243d7a74bd2fb9
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-04-25 16:54:27 +0530

    Boiler Plate Commit

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports.authController = require("./auth.controller");
+module.exports.errorController = require("./error.controller");
+module.exports.userController = require("./user.controller");
