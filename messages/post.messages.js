5b94507133784fffd84451dc892f751152107668
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-04-25 19:01:56 +0530

    tuesday commit: post remining

---
+++ messages/post.messages.js
@@ -0,0 +1,13 @@
+module.exports = {
+  error: {
+    POST_NOT_FOUND: "user not found!",
+    INVALID_UPDATES: "Invalid Updates",
+  },
+  success: {
+    POST_CREATION_SUCCESS: "post created Successfully!",
+    POST_FETCH_SUCCESS: "post fetched successfully!",
+    POSTS_FETCH_SUCCESS: "posts fetched successfully!",
+    POST_UPDATION_SUCCESS: "post updated successfully!",
+    POST_DELETION_SUCCESS: "post deleted successfully!",
+  },
+};
